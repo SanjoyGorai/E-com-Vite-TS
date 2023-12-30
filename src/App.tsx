df3c2c49a84987e46e1536e32d268fa726b97c16
Mr. Sanjoy <--add>
2023-12-30 12:37:32 +0530

    router is created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './styles/app.scss'
+import Home from './pages/Home.tsx'
+import Search from './pages/Search.tsx'
+import Cart from './pages/Cart.tsx'
 
 const App = () => {
   return (
     <Router>
       {/* Header */}
       <Routes>
-        <Route />
+        <Route path='/' element={<Home/>} />
+        <Route path='/search' element={<Search/>} />
+        <Route path='/cart' element={<Cart/>} />
       </Routes>
     </Router>
   )
